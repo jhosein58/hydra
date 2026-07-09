69866ae53fbc6c5136550425e4c053ce110722b3
jhosein58 <jhosein58@gmail.com>
2026-07-09 03:35:51 +0330

    new AHMETN banner

--- hydra-core/src/main.rs
+++ hydra-core/src/main.rs
@@ -3,25 +3,19 @@ pub mod crypto;
 pub mod identity;
 
 use axum::{Router, routing::get};
-use serde::{Deserialize, Serialize};
-
-use crate::auth::jwt::Jwt;
-
-#[derive(Serialize, Deserialize)]
-struct Claims {
-    name: String,
-}
+use figlet_rs::FIGlet;
 
 #[tokio::main]
 async fn main() {
-    let my_claims = Claims {
-        name: "AHMADI".to_string(),
-    };
-
-    println!("{}", Jwt::encode(&my_claims, "test").unwrap());
+    let standard_font = FIGlet::standard().unwrap();
+    println!("{}", standard_font.convert("AHMENT").unwrap());
 
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3400").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("localhost:3000")
+        .await
+        .unwrap();
+
+    println!("   Server started at: http://localhost:3000");
     axum::serve(listener, app).await.unwrap();
 }
